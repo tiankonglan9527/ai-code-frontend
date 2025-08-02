3cd2bf792927ea6f462b0f64b851822bbd155577
JackiyWong <1903915989@qq.com>
2025-08-02 21:00:12 +0800

    前端初始化完成

---
+++ yu-ai-code-mother-frontend/src/api/healthController.ts
@@ -0,0 +1,19 @@
+// @ts-ignore
+/* eslint-disable */
+import request from '@/request'
+
+/** 此处后端没有提供注释 GET /health/ */
+export async function healthCheck(options?: { [key: string]: any }) {
+  return request<API.BaseResponseString>('/health/', {
+    method: 'GET',
+    ...(options || {}),
+  })
+}
+
+/** 此处后端没有提供注释 GET /health/healthCheck */
+export async function healthCheck1(options?: { [key: string]: any }) {
+  return request<string>('/health/healthCheck', {
+    method: 'GET',
+    ...(options || {}),
+  })
+}
